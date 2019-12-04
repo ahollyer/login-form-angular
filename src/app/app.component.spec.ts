f7e330feb90f4d9bdb2fe84498cc34b9888d20d6
Aspen Hollyer <aspen.hollyer@gmail.com>
2019-12-04 11:49:28 -0600

    Delete unused boilerplate tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,16 +19,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  // it(`should render login form component`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app.title).toEqual('login-form-angular');
-  // });
-
-  // it('should render title in a h1 tag', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('h1').textContent).toContain('Welcome to login-form-angular!');
-  // });
 });
