f93edbed1a80bbc9e9d19919b4c21a312db74cc2
Aspen Hollyer <aspen.hollyer@gmail.com>
2019-12-04 11:34:57 -0600

    Create reactive login form, validation, error messages

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login-form',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor() { }
+  constructor( private fb: FormBuilder,
+               private userService: UserService ) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  loginForm = this.fb.group({
+    /*
+    NOTE: Angular's built-in email validator is based on the WHATWG spec:
+    https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address
+
+    If problems are reported, a more permissive validator may be necessary. See
+    https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+    */
+    email: ['', [ Validators.required, Validators.email ]],
+    password: ['', [ Validators.required, Validators.minLength(8) ]],
+  })
+
+  /** Getter for easy access to form fields in the template */
+  get f() {
+    return this.loginForm.controls;
+  }
+
+
+  /** Sends login credentials to user service */
+  onSubmit(): void {
+    if(!this.loginForm.valid) return;
+
+    this.userService.login(this.loginForm.value).subscribe(
+      res => {
+        // TODO: Handle success
+        console.log("Got response", res)
+      },
+      err => {
+        // TODO: Handle error
+        console.error("Got error", err)
+      }
+    )
   }
 
 }
